ef0c26f0a6b8c19858d30142c95f4aa16f1ac1b5
DuyMTA <59283692+DuyMTA@users.noreply.github.com>
2020-12-03 13:55:15 +0700

    Update FrmBanHang.cs
     ...

--- QLBanHang/GUI/FrmBanHang.cs
+++ QLBanHang/GUI/FrmBanHang.cs
@@ -97,6 +97,7 @@ namespace QuanLyHangHoa.GUI
         #region sự kiện phiếu xuất
         private void btnStart_Click(object sender, EventArgs e)
         {
+            btnStart.BackColor = Color.Red;
             index = 0;
             LoadPhieuXuat();
         }
