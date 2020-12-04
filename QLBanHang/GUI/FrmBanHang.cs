72e7031ee8a864e99d30f65f99ffbe3e3b271093
DuyMTA <59283692+DuyMTA@users.noreply.github.com>
2020-12-04 14:30:40 +0700

    Update FrmBanHang.cs
    update comment

--- QLBanHang/GUI/FrmBanHang.cs
+++ QLBanHang/GUI/FrmBanHang.cs
@@ -125,6 +125,7 @@ namespace QuanLyHangHoa.GUI
         #region sự kiện thêm sửa xóa phiếu xuất
         private void btnThemPhieuXuat_Click(object sender, EventArgs e)
         {
+        	//duy change this
            btnThemPhieuXuat.BackColor=Color.Red;
             XUAT xuat = new XUAT();
             xuat.NHANVIENID = (int) cbxNhanVien.SelectedValue;
